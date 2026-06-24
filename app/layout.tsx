b78fea421e70c9c41238339b05a5a1b9e8d72c16
Gautam Punj <punj.sunny@gmail.com>
2026-06-24 13:15:32 +0530

    Use inline script tag for GTM to appear in view-source

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { CookieConsentBanner } from "@/components/CookieConsentBanner";
 import { TrackingScripts } from "@/components/TrackingScripts";
-import Script from "next/script";
 import "./globals.css";
 
 const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
@@ -25,13 +24,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {GTM_ID && (
-          <Script id="gtm-head" strategy="beforeInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}
-          </Script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`
+            }}
+          />
         )}
       </head>
       <body>
