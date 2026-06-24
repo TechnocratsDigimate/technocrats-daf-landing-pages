40a1facb98f614cafaf5b56e5de814d4eb8ffe14
Gautam Punj <punj.sunny@gmail.com>
2026-06-24 14:17:35 +0530

    Load GTM immediately without consent gating

--- components/TrackingScripts.tsx
+++ components/TrackingScripts.tsx
@@ -27,65 +27,38 @@ export function TrackingScripts() {
   }, []);
 
   useEffect(() => {
-    if (!trackingEnabled) {
-      return;
-    }
-
-    const timer = window.setTimeout(() => {
-      trackPageView();
-    }, 600);
-
+    if (!trackingEnabled) return;
+    const timer = window.setTimeout(() => { trackPageView(); }, 600);
     return () => window.clearTimeout(timer);
   }, [trackingEnabled]);
 
-  if (!trackingEnabled) {
-    return null;
-  }
-
   return (
     <>
+      {/* GTM — loads immediately, no consent required */}
       {gtmId ? (
         <Script id="gtm-base" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`}
         </Script>
       ) : null}
 
-      {ga4Id ? (
+      {/* GA4 + Meta Pixel — only after consent */}
+      {trackingEnabled ? (
         <>
-          <Script src={`https://www.googletagmanager.com/gtag/js?id=${ga4Id}`} strategy="afterInteractive" />
-          <Script id="ga4-base" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              window.gtag = gtag;
-              gtag('js', new Date());
-              gtag('config', '${ga4Id}', { send_page_view: false });
-            `}
-          </Script>
-        </>
-      ) : null}
+          {ga4Id ? (
+            <>
+              <Script src={`https://www.googletagmanager.com/gtag/js?id=${ga4Id}`} strategy="afterInteractive" />
+              <Script id="ga4-base" strategy="afterInteractive">
+                {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;gtag('js',new Date());gtag('config','${ga4Id}',{send_page_view:false});`}
+              </Script>
+            </>
+          ) : null}
 
-      {metaPixelId ? (
-        <Script id="meta-pixel-base" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${metaPixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+          {metaPixelId ? (
+            <Script id="meta-pixel-base" strategy="afterInteractive">
+              {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${metaPixelId}');fbq('track','PageView');`}
+            </Script>
+          ) : null}
+        </>
       ) : null}
     </>
   );
