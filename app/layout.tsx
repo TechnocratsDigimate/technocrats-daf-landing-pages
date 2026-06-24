43d8a3982db172585ba0f0310489a1f0e87cdffb
Gautam Punj <punj.sunny@gmail.com>
2026-06-24 13:11:14 +0530

    Add GTM directly in root layout without consent gating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { CookieConsentBanner } from "@/components/CookieConsentBanner";
 import { TrackingScripts } from "@/components/TrackingScripts";
+import Script from "next/script";
 import "./globals.css";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://technocratsdigimate.com"),
@@ -20,7 +23,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {GTM_ID && (
+          <Script id="gtm-head" strategy="beforeInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+          </Script>
+        )}
+      </head>
       <body>
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
         <TrackingScripts />
         {children}
         <CookieConsentBanner />
